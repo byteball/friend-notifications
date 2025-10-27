367ea4b704817fd691408e2d37fb7ed954aadfd8
Taump <taump@yandex.ru>
2025-10-27 16:29:03 +0100

    refactor(chatBot): improve ghost name handling logic

--- chatBot/index.js
+++ chatBot/index.js
@@ -7,6 +7,8 @@ const { isValidAddress } = require('ocore/validation_utils');
 const aa_state = require('aabot/aa_state.js');
 const getSignedData = require('../utils/getSignedData');
 
+const MAX_PAIRING_KEY_LENGTH = 73;
+
 // Cache device address -> selected ghost name
 const deviceAddressToGhostName = new LRUCache({
 	max: 1000,
@@ -17,40 +19,43 @@ const deviceAddressToGhostName = new LRUCache({
 
 const startChatBot = () => {
 	eventBus.on('paired', async (from_address, ghostName) => {
-		ghostName = ghostName.trim().slice(0, 40); // max 40 chars
+		const vars = aa_state.getAAStateVars(conf.friend_aa);
+		[ghostName, address] = ghostName.trim().slice(0, MAX_PAIRING_KEY_LENGTH).split("_"); // max 40 chars + address + 1 for split
 
 		let device = require('ocore/device');
-		if (isValidAddress(ghostName)) return device.sendMessageToDevice(from_address, 'text', 'Invalid ghost name');
 
-		const vars = aa_state.getAAStateVars(conf.friend_aa);
+		if (isValidAddress(ghostName)) return device.sendMessageToDevice(from_address, 'text', 'Invalid ghost name');
+		if (!isValidAddress(address)) return device.sendMessageToDevice(from_address, 'text', 'Invalid wallet address');
+
+		const key = `friendship_${ghostName}_${address}`;
 
 		if (!vars || !vars[`user_${ghostName}`]) return device.sendMessageToDevice(from_address, 'text', 'Unknown ghost name');
+		if (vars[key]) return device.sendMessageToDevice(from_address, 'text', 'We\'re already friends! No need to link again.');
 
 		deviceAddressToGhostName.set(from_address, ghostName);
 
-		device.sendMessageToDevice(from_address, 'text', 'Please send me your address that you wish to assign with ghost (click ... and Insert my address)');
+		return device.sendMessageToDevice(from_address, 'text', `Please sign this message to choose ${ghostName} as your future friend at the end of the current streak: [${address}](sign-message-request: choose ${ghostName} as ${address} future friend)`);
 	});
 
 
 	eventBus.on('text', async (from_address, text) => {
 		text = text.trim();
+		
 		let device = require('ocore/device');
-
-		const selectedGhost = deviceAddressToGhostName.get(from_address);
+		
+		const selectedGhost = deviceAddressToGhostName.get(from_address).trim();
 
 		if (!selectedGhost) return device.sendMessageToDevice(from_address, 'text', 'Please select a ghost name on the website first.');
 
-		if (isValidAddress(text)) {
-			return device.sendMessageToDevice(from_address, 'text', `Please sign this message to prove that you own the address: [${text}](sign-message-request: I own the address: ${text}. And I select ghost: ${selectedGhost}).`);
-		} else if (text.trim().startsWith("[Signed message]") && selectedGhost) {
+		if (text.trim().startsWith("[Signed message]") && selectedGhost) {
 			let signedData;
-			
+
 			try {
 				signedData = await getSignedData(from_address, text);
 			} catch (error) {
 				return device.sendMessageToDevice(from_address, 'text', "Failed to retrieve signed data");
 			}
-
+			
 			if (signedData?.deviceAddress !== from_address) {
 				return device.sendMessageToDevice(from_address, 'text', "Unknown device address in the signed message");
 			}
@@ -59,7 +64,7 @@ const startChatBot = () => {
 				return device.sendMessageToDevice(from_address, 'text', `Please return to the website and select the ghost again.`);
 			}
 
-			const address = signedData.attestationWalletAddress;
+			const address = signedData.walletAddress;
 
 			await db.query("REPLACE INTO user_ghost (address, ghost_name) VALUES(?,?)", [address, selectedGhost]);
 
