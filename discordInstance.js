c0a8f8e610da11a00245701519787ffbdcffd93b
Taump <taump@yandex.ru>
2025-11-06 15:51:49 +0100

    fix: move 2

--- discordInstance.js
+++
@@ -1,6 +0,0 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-
-const discordInstance = new Client({ allowedMentions: { parse: ['users'] }, intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds] });
-
-module.exports = discordInstance;
-
